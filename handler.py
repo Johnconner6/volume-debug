d2a701b5c4ed173bcf8b8cd22e05405de00476d8
Johnconner6 <someonestole54@gmail.com>
2026-05-30 16:16:07 +0200

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,18 +1,6 @@
-import os
-import runpod
+import os, runpod
 
 def handler(job):
-    job_input = job.get("input", {})
-    result = {}
-    result["volume_exists"] = os.path.exists("/runpod-volume")
-    try:
-        result["volume_contents"] = os.listdir("/runpod-volume")
-    except Exception as e:
-        result["volume_contents"] = str(e)
-    try:
-        result["models_contents"] = os.listdir("/runpod-volume/models")
-    except Exception as e:
-        result["models_contents"] = str(e)
-    return result
+    return {"files": os.listdir("/runpod-volume/models")}
 
 runpod.serverless.start({"handler": handler})
